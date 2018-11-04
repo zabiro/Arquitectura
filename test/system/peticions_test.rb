7a84d0b4218e0bfe058f3f371a4158ee867fa394
luciano <lucianoadb@gmail.com>
2018-11-04 16:49:40 -0500

    Creacion del primer CRUD con Scaffold

---
+++ test/system/peticions_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PeticionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit peticions_url
+  #
+  #   assert_selector "h1", text: "Peticion"
+  # end
+end
